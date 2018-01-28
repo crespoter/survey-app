b45cb1e7840ee1dba0e142dc4d3821f92d7d5979
crespoter <davidchristiek@gmail.com>
2018-01-29 00:12:12 +0530

    added toolbar

--- app/src/main/java/com/survey/iiits/survey_iiits/MainActivity.java
+++ app/src/main/java/com/survey/iiits/survey_iiits/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout dLayout;
@@ -13,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // get the reference of Toolbar
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+
+
         dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navView = (NavigationView) findViewById(R.id.navigation);
         Fragment def = new Home();
