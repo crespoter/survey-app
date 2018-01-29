3e2fd474e4f61d3af41f168367bfbe6d68e3ba22
shyam <shyamsunder.p15@iiits.in>
2018-01-30 03:32:21 +0530

    Drafts

---
+++ app/src/main/java/com/survey/iiits/survey_iiits/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.survey.iiits.survey_iiits;
+
+/**
+ * Created by user on 29/01/2018.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ImageView selectedImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        selectedImage = (ImageView) findViewById(R.id.selectedImage); // init a ImageView
+        Intent intent = getIntent(); // get Intent which we set from Previous Activity
+        selectedImage.setImageResource(intent.getIntExtra("image", 0)); // get image from Intent and set it in ImageView
+    }
+}
