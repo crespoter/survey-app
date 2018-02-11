dd55b9aef2c9d650c472cc55a15e6459b8273b15
shyam <shyamsunder.p15@iiits.in>
2018-02-11 23:25:03 +0530

    updated FAB

--- app/src/main/java/com/survey/iiits/survey_iiits/CreateSurveyActivity.java
+++ app/src/main/java/com/survey/iiits/survey_iiits/CreateSurveyActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 
 public class CreateSurveyActivity extends AppCompatActivity {
     createSurveyAdapter adapter;
     RecyclerView recyclerView;
     DrawerLayout dLayout;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab,fab1,fab2;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +28,23 @@ public class CreateSurveyActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         adapter = new createSurveyAdapter(getApplicationContext());
         recyclerView.setAdapter(adapter);
-        FloatingActionButton addButton = (FloatingActionButton)findViewById(R.id.createSurvey_addIcon);
-        addButton.setOnClickListener(new View.OnClickListener() {
+
+
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateFAB();
+
+            }
+        });
+        fab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("HHERE", "onClick:CLICKED FOB");
@@ -33,5 +53,28 @@ public class CreateSurveyActivity extends AppCompatActivity {
             }
         });
     }
+    public void animateFAB(){
 
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+
+
+        } else {
+
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+            Log.d("Raj","open");
+
+        }
+    }
 }
