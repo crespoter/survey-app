926c33053faf002081cd0ead7fffcb0e5d0fc6fd
shyam <pavankumar>
2018-05-30 04:45:32 +0530

    Update 2.0

--- app/src/main/java/com/survey/iiits/survey_iiits/NewSurveyCheckerService.java
+++ app/src/main/java/com/survey/iiits/survey_iiits/NewSurveyCheckerService.java
@@ -7,16 +7,23 @@ package com.survey.iiits.survey_iiits;
  */
 
 
+import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import android.view.WindowManager;
+import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -110,13 +117,17 @@ public class NewSurveyCheckerService extends Service {
             String note;
             String timestamp;
             String title;
-            Message(int id,String userName,String title,String note,String timestamppost)
+            int forcedResponse;
+            int anonymouse;
+            Message(int id,String userName,String title,String note,String timestamppost,int forcedResponse,int anonymouse)
             {
                 this.id = id;
                 this.senderName = userName;
                 this.note = note;
                 this.title = title;
                 this.timestamp = timestamppost;
+            this.forcedResponse=forcedResponse;
+            this.anonymouse=anonymouse;
             }
         }
         ArrayList<Message> messages = new ArrayList<>();
@@ -124,16 +135,17 @@ public class NewSurveyCheckerService extends Service {
         @Override
         protected void onPostExecute(String s) {
             int messageNumber = 0;
+            int countnum=0;
             for(int i=0;i<messages.size();i++)
             {
-                Intent intent = new Intent(getBaseContext(), Questionaire.class);
+               if(messages.get(i).forcedResponse==0){ Intent intent = new Intent(getBaseContext(), Questionaire.class);
                 intent.putExtra("firstKeyName","" + messages.get(i).id);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 PendingIntent pIntent = PendingIntent.getActivity(NewSurveyCheckerService.this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
                 String CHANNEL_ID = "my_channel_01";
                 Notification n  = new Notification.Builder(NewSurveyCheckerService.this)
                         .setContentText("New Survey from : " + messages.get(i).senderName)
-                        .setContentTitle("Sender :" + messages.get(i).senderName + "  Note : " + messages.get(i).note)
+                        .setContentTitle("Topic :" + messages.get(i).title + "  Note : " + messages.get(i).note)
                         .setSmallIcon(R.drawable.draft_icon)
                         .setContentIntent(pIntent)
                         .setAutoCancel(true)
@@ -143,6 +155,49 @@ public class NewSurveyCheckerService extends Service {
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 messageNumber++;
                 notificationManager.notify(messageNumber, n);
+            }else {
+                   if (countnum == 0) {
+                       countnum++;
+                       final int temp = i;
+                       new Handler(Looper.getMainLooper()).post(new Runnable() {
+                           @Override
+                           public void run() {
+                               Toast.makeText(getApplicationContext(),
+                                       "hai",
+                                       Toast.LENGTH_SHORT).show();
+
+                               final AlertDialog alertDialog = new AlertDialog.Builder(NewSurveyCheckerService.this)
+                                       .setTitle("Urgent Response Alert")
+                                       .setCancelable(false)
+                                       .setMessage("NewSurvey from" + messages.get(temp).senderName + "\n topic :" + messages.get(temp).title).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                           public void onClick(DialogInterface dialog, int which) {
+                                               // continue with delete
+                                                 Intent intent2 = new Intent(getBaseContext(), Questionaire.class);
+                                               intent2.putExtra("firstKeyName","" + messages.get(temp).id);
+                                               intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                               Intent intent = new Intent(NewSurveyCheckerService.this, NewSurveyCheckerService.class);
+
+                                               stopService(intent);
+
+                                               startActivity(intent2);
+
+
+                                           }
+                                       })
+                                       .create();
+
+                               alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+                               if (alertDialog.isShowing()) {
+                                   Log.d("hai", "pritntoast" + temp + isServiceRunning());
+                               } else {
+                                   alertDialog.show();
+
+                               }
+                           }
+                       });
+
+                   }
+               }
             }
 
         }
@@ -160,6 +215,7 @@ public class NewSurveyCheckerService extends Service {
                 {
                     jsonResponse += inputLine;
                 };
+                Log.d("hai",isServiceRunning()+"running");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -170,7 +226,7 @@ public class NewSurveyCheckerService extends Service {
                 for(int i=0;i<jsonMessageList.length();i++)
                 {
                     JSONObject row = jsonMessageList.getJSONObject(i);
-                    messages.add(new Message(row.getInt("idquestionnaire"),row.getString("username"),row.getString("title"), row.getString("note"),row.getString("timestamppost")));
+                    messages.add(new Message(row.getInt("idquestionnaire"),row.getString("username"),row.getString("title"), row.getString("note"),row.getString("timestamppost"),row.getInt("forcedResponse"),row.getInt("anonymous")));
                     //Message message = new Message(1,"CRESPOTER","TESTTT", "TESTNOTE","DATETIME");
                     //messages.add(message);
 
@@ -180,6 +236,15 @@ public class NewSurveyCheckerService extends Service {
             }
             return null;
         }
+    }
+    private boolean isServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if("com.survey.iiits.survey_iiits.NewSurveyCheckerService".equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
